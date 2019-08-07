db9bcc83bc4f43991f1d366fd08644777316473a
K2User <K2User@DESKTOP-ECSMUSF>
2019-08-07 11:57:43 +0800

    add test case

---
+++ src/test/java/xClubsys/LifeConversion.java
@@ -0,0 +1,114 @@
+package xClubsys;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Stories;
+import io.qameta.allure.Story;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.*;
+
+@Feature("Membership")
+@Owner("Pisy")
+@Stories(value = { @Story(value = "Membership") })
+public class LifeConversion extends config {
+	@Test
+	public void ConvertToLifMembership() throws InterruptedException {
+		driver.get(GetBaseUrl() + "/Membership/Members/ConvertMembership");
+		sleep(5);
+		assertEquals(driver
+				.findElement(By.xpath(
+						"(.//*[normalize-space(text()) and normalize-space(.)='Life Conversion'])[2]/following::h2[1]"))
+				.getText(), "Convert To Life Membership");
+		assertEquals(driver.findElement(By.xpath("//md-tab-item")).getText(), "CONVERSION");
+		assertEquals(driver
+				.findElement(By.xpath(
+						"(.//*[normalize-space(text()) and normalize-space(.)='Member Name'])[1]/following::span[2]"))
+				.getText(), "Member ID");
+		assertEquals(driver
+				.findElement(By.xpath(
+						"(.//*[normalize-space(text()) and normalize-space(.)='Member ID'])[1]/following::span[1]"))
+				.getText(), "Name");
+		assertEquals(driver
+				.findElement(
+						By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Name'])[1]/following::span[1]"))
+				.getText(), "From Membership Type");
+		assertEquals(driver.findElement(By.xpath(
+				"(.//*[normalize-space(text()) and normalize-space(.)='From Membership Type'])[1]/following::span[1]"))
+				.getText(), "To Membership Type");
+		assertEquals(driver.findElement(By.xpath(
+				"(.//*[normalize-space(text()) and normalize-space(.)='To Membership Type'])[1]/following::span[1]"))
+				.getText(), "Conversion Fee");
+		assertEquals(driver.findElement(By
+				.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Conversion Fee'])[1]/following::span[1]"))
+				.getText(), "Transaction Date");
+		assertEquals(driver.findElement(By.xpath(
+				"(.//*[normalize-space(text()) and normalize-space(.)='Transaction Date'])[1]/following::span[1]"))
+				.getText(), "Effective Date");
+		assertEquals(driver.findElement(By
+				.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Effective Date'])[1]/following::span[1]"))
+				.getText(), "Status");
+		assertEquals(driver.findElement(By.xpath("//md-tab-item[2]")).getText(), "ELIGIBLE MEMBER");
+		assertEquals(driver.findElement(By.xpath("//div[2]/div/a")).getText(), "NEW LIFE MEMBERSHIP");
+	}
+	@Test
+	public void ConvertToLifeMembership() throws InterruptedException {
+		driver.get(GetBaseUrl() + "/Membership/Members/ConvertMembership/Create");
+		sleep(5);
+		
+		assertEquals(driver.findElement(By.xpath(
+				"(.//*[normalize-space(text()) and normalize-space(.)='Membership Type is required.'])[1]/following::span[1]"))
+				.getText(), "Transaction");
+		assertEquals(driver
+				.findElement(By.xpath(
+						"(.//*[normalize-space(text()) and normalize-space(.)='Remarks'])[1]/following::button[1]"))
+				.getText(), "SAVE AS DRAFT");
+		assertEquals(driver.findElement(By.xpath(
+				"(.//*[normalize-space(text()) and normalize-space(.)='Save as Draft'])[1]/following::button[1]"))
+				.getText(), "SUBMIT");
+		assertEquals(driver
+				.findElement(By.xpath(
+						"(.//*[normalize-space(text()) and normalize-space(.)='Submit'])[1]/following::button[1]"))
+				.getText(), "CANCEL");
+		assertEquals(driver.findElement(By.cssSelector("h2.ng-binding")).getText(), "Convert To Life Membership");
+		assertEquals(driver.findElement(By.xpath(
+				"//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[1]/div/div[1]/md-input-container[1]/label[@class='ng-scope']"))
+				.getText(), "Member ID");
+		assertEquals(driver.findElement(By.xpath(
+				"//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[1]/div/div[1]/md-input-container[2]/label[@class='ng-scope']"))
+				.getText(), "Member Name");
+		assertEquals(driver.findElement(By.xpath(
+				"//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[1]/div/div[1]/md-input-container[3]/label[@class='ng-scope']"))
+				.getText(), "Original Membership Type");
+		assertEquals(driver.findElement(By.xpath(
+				"//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[1]/div/div[2]/md-input-container/md-select[@role='listbox']"))
+				.getText(), "Membership Type");
+		assertEquals(driver.findElement(By.xpath(
+				"//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[2]/div/div[1]/md-input-container[1]/md-select[@role='listbox']"))
+				.getText(), "Subscription Fee Type");
+		assertEquals(driver.findElement(By.xpath(
+				"//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[2]/div/div[1]/md-input-container[2]/label[@class='ng-scope']"))
+				.getText(), "Reduced Subscription Fee");
+		assertEquals(driver.findElement(By.xpath(
+				"//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[2]/div/div[1]/md-input-container[3]/label[.='Life Membership Conversion Fee']"))
+				.getText(), "Life Membership Conversion Fee");
+		assertEquals(driver.findElement(By.xpath(
+				"//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[2]/div/div[2]/md-input-container[1]/label[@class='ng-scope']"))
+				.getText(), "Generate Conversion Debit Note");
+		assertEquals(driver.findElement(By.xpath(
+				"//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[2]/div/div[2]/md-input-container[2]/label[.='Transaction Date']"))
+				.getText(), "Transaction Date");
+		assertEquals(driver.findElement(By.xpath(
+				"//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[2]/div/div[2]/md-input-container[3]/label[.='Effective Date']"))
+				.getText(), "Effective Date");
+		assertEquals(driver.findElement(By.xpath(
+				"//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[2]/div/div[3]/md-input-container/label[@class='ng-scope']"))
+				.getText(), "Remarks");
+		assertEquals(driver.findElement(By.xpath(
+				"(.//*[normalize-space(text()) and normalize-space(.)='Convert To Life Membership'])[1]/following::span[1]"))
+				.getText(), "Member");
+	}
+}
---
+++ src/test/java/xClubsys/Transfer.java
@@ -0,0 +1,90 @@
+package xClubsys;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Stories;
+import io.qameta.allure.Story;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.*;
+
+@Feature("Membership")
+@Owner("Pisy")
+@Stories(value = { @Story(value = "Membership") })
+public class Transfer extends config {
+	@Test
+	public void MembershipTransferHistory() throws InterruptedException {
+		driver.get(GetBaseUrl() + "/Membership/Members/MTransfer/List");
+		sleep(5);
+		assertEquals(driver.findElement(By.cssSelector("a.md-warn.md-button.md-ink-ripple")).getText(),
+				"NEW MEMBERSHIP TRANSFER");
+		assertEquals(driver
+				.findElement(By
+						.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Transfer'])[2]/following::h2[1]"))
+				.getText(), "Membership Transfer History");
+		assertEquals(driver
+				.findElement(
+						By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Done'])[1]/following::span[2]"))
+				.getText(), "From Member (Name/ID)");
+		assertEquals(driver.findElement(By.xpath(
+				"(.//*[normalize-space(text()) and normalize-space(.)='From Member (Name/ID)'])[1]/following::span[1]"))
+				.getText(), "To Member (Name/ID)");
+		assertEquals(driver.findElement(By.xpath(
+				"(.//*[normalize-space(text()) and normalize-space(.)='To Member (Name/ID)'])[1]/following::span[1]"))
+				.getText(), "Transaction Date");
+		assertEquals(driver.findElement(By.xpath(
+				"(.//*[normalize-space(text()) and normalize-space(.)='Transaction Date'])[1]/following::span[1]"))
+				.getText(), "Effective Date");
+		assertEquals(driver.findElement(By
+				.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Effective Date'])[1]/following::span[1]"))
+				.getText(), "Transfer Fee");
+		assertEquals(driver
+				.findElement(By.xpath(
+						"(.//*[normalize-space(text()) and normalize-space(.)='Transfer Fee'])[1]/following::span[1]"))
+				.getText(), "Admin Fee");
+		assertEquals(driver
+				.findElement(By.xpath(
+						"(.//*[normalize-space(text()) and normalize-space(.)='Admin Fee'])[1]/following::span[1]"))
+				.getText(), "Status");
+	}
+	@Test
+	public void CreateMembershipTransfer() throws InterruptedException {
+		driver.get(GetBaseUrl() + "/Membership/Members/MTransfer/Create");
+		sleep(5);
+	    assertEquals(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Transfer'])[2]/following::h2[1]")).getText(), "Create Membership Transfer");
+	    assertEquals(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Create Membership Transfer'])[1]/following::h4[1]")).getText(), "From Member");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[1]/md-card-content//label[.='Member ID']")).getText(), "Member ID");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[1]/md-card-content/div[1]/md-input-container[2]/label[@class='ng-scope']")).getText(), "Name");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[1]/md-card-content/div[1]/md-input-container[3]/label[@class='ng-scope']")).getText(), "Membership Type");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[1]/md-card-content//label[.='Outstanding Balance']")).getText(), "Outstanding Balance");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[1]/md-card-content//label[.='Refundable Deposit']")).getText(), "Refundable Deposit");
+		/*
+		 * assertEquals(driver.findElement(By.
+		 * cssSelector("[az-name] md-card-content:nth-of-type(2) h4")).getText(),
+		 * "To Member\r\n" + "add");
+		 */
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[2]//label[.='Member ID']")).getText(), "Member ID");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[2]/div/md-input-container[2]/label[@class='ng-scope']")).getText(), "Name");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[2]/div/md-input-container[3]/label[@class='ng-scope']")).getText(), "Membership Type");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//md-card[@class='_md']/md-card-content[3]/h4[.='Transaction']")).getText(), "Transaction");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//md-card[@class='_md']/md-card-content[3]/div[1]/md-input-container[1]/md-select[@role='listbox']/md-select-value/span[.='Transfer Reason / Remarks']")).getText(), "Transfer Reason / Remarks");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//md-card[@class='_md']/md-card-content[3]/div[1]/md-input-container[3]/md-select[@role='listbox']/md-select-value/span[.='Transfer Fee Invoice Account']")).getText(), "Transfer Fee Invoice Account");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[3]/div[2]/md-input-container[1]/label[@class='ng-scope']")).getText(), "Transfer Fee");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[3]/div[2]/md-input-container[2]/label[@class='ng-scope']")).getText(), "Generate Transfer Fee invoice");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[3]/div[2]/md-input-container[3]/label[@class='ng-scope']")).getText(), "Transferee Credit Deposit Fee");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[3]//md-datepicker[@name='TransactionDate']//input[@class='md-datepicker-input md-input']")).getText(), "Transaction Date");
+	    assertEquals(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Remarks'])[1]/following::h4[1]")).getText(), "Attachment List");
+	    assertEquals(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='file_upload'])[1]/following::span[1]")).getText(), "UPLOAD");
+	    assertEquals(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Upload'])[1]/following::span[2]")).getText(), "File Name");
+	    assertEquals(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='File Name'])[1]/following::span[1]")).getText(), "Document Description");
+	    assertEquals(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Document Description'])[1]/following::span[1]")).getText(), "Date Uploaded");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[3]//md-datepicker[@name='EffectiveDate']//input[@class='md-datepicker-input md-input']")).getText(), "Effective Date");
+	    assertEquals(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Date Uploaded'])[1]/following::button[1]")).getText(), "SAVE AS DRAFT");
+	    assertEquals(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Save as Draft'])[1]/following::button[1]")).getText(), "SUBMIT");
+	    assertEquals(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Submit'])[1]/following::button[1]")).getText(), "CANCEL");
+	    assertEquals(driver.findElement(By.xpath("//div[@id='main']/div[1]/div//form[@name='form1']/md-card/md-card-content[3]/div[1]/md-input-container[2]/label[@class='ng-scope']")).getText(), "Transfer Price");
+		}
+}
